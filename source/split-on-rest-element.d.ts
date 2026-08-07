dcbbf35edb7f2fdf11878cb441c5c27aa0cac89c
airismadeupofair <airismadeupofair@gmail.com>
2026-08-07 14:39:54 +0100

    Add a faster implementation for SplitOnRestElement

--- source/split-on-rest-element.d.ts
+++ source/split-on-rest-element.d.ts
@@ -3,6 +3,7 @@ import type {ApplyDefaultOptions} from './internal/object.d.ts';
 import type {IsOptionalKeyOf} from './is-optional-key-of.d.ts';
 import type {IsArrayReadonly} from './internal/array.d.ts';
 import type {UnknownArray} from './unknown-array.d.ts';
+import type {TupleOf} from './tuple-of.d.ts';
 import type {If} from './if.d.ts';
 
 /**
@@ -75,34 +76,147 @@ export type SplitOnRestElement<
 			: never // Should never happen
 		: never; // Should never happen
 
-/**
-Deconstructs an array on its rest element and returns the split portions.
-*/
+type Required1000UnknownElements = TupleOf<1000>
+type Required100UnknownElements = TupleOf<100>
+type Required10UnknownElements = TupleOf<10>
+
+type Optional1000UnknownElements = Partial<Required1000UnknownElements>;
+type Optional100UnknownElements = Partial<Required100UnknownElements>;
+type Optional10UnknownElements = Partial<Required10UnknownElements>;
+
+// because ts doesnt support doing infer from multiple rest elements a double inference is needed to extract the first N elements and the rest
+type GetAllRequiredElementsOnLeftSide<Array_ extends UnknownArray, Accumulator extends UnknownArray = []> =
+	// Get the first 1000 required elements
+    Array_ extends {[999]: unknown}
+        ? Array_ extends readonly [...infer ThousandRequiredElements extends Required1000UnknownElements, ...any] // pull out the elements using the constraint of a fixed tuple
+            ? Array_ extends readonly [...Required1000UnknownElements, ...infer RestElements]
+                ? GetAllRequiredElementsOnLeftSide<RestElements, [...Accumulator, ...ThousandRequiredElements]>
+                : never // should not here because case above would fail
+            : never // should not here because case above would fail
+    
+    // Get the first 100
+    : Array_ extends {[99]: unknown}
+        ? Array_ extends readonly [...infer HundredRequiredElements extends Required100UnknownElements, ...any] // pull out the elements using the constraint of a fixed tuple
+            ? Array_ extends readonly [...Required100UnknownElements, ...infer RestElements]
+                ? GetAllRequiredElementsOnLeftSide<RestElements, [...Accumulator, ...HundredRequiredElements]>
+                : never // should not here because case above would fail
+            : never // should not here because case above would fail
+    
+    // get the first 10
+    : Array_ extends {[9]: unknown}
+        ? Array_ extends readonly [...infer TenRequiredElements extends Required10UnknownElements, ...any] // pull out the elements using the constraint of a fixed tuple
+            ? Array_ extends readonly [...Required10UnknownElements, ...infer RestElements]
+                ? GetAllRequiredElementsOnLeftSide<RestElements, [...Accumulator, ...TenRequiredElements]>
+                : never // should not here because case above would fail
+            : never // should not here because case above would fail
+    
+    // just get the first element
+    : Array_ extends readonly [infer RequiredElement, ...infer RestElements]
+    ? GetAllRequiredElementsOnLeftSide<RestElements, [...Accumulator, RequiredElement]>
+    : [Accumulator, Array_]
+	
+// because ts doesnt support doing infer from multiple rest elements a double inference is needed to extract the first N elements and the rest.
+type GetAllOptionalElementsOnLeftSide<Array_ extends UnknownArray, Accumulator extends UnknownArray = []> =
+    // ge the first 1000 items
+    Array_ extends {[999]?: unknown}
+        ? Array_ extends readonly [...infer ThousandOptionalElements extends Optional1000UnknownElements, ...any]
+            ? Array_ extends readonly [...Optional1000UnknownElements, ...infer RestElements]
+                ? GetAllOptionalElementsOnLeftSide<RestElements, [...Accumulator, ...ThousandOptionalElements]>
+                : never
+            : never
+
+    // get the first 100 items
+    : Array_ extends {[99]?: unknown}
+        ? Array_ extends readonly [...infer HundredOptionalElements extends Optional100UnknownElements, ...any]
+            ? Array_ extends readonly [...Optional100UnknownElements, ...infer RestElements]
+                ? GetAllOptionalElementsOnLeftSide<RestElements, [...Accumulator, ...HundredOptionalElements]>
+                : never
+            : never
+			
+    // get the first 10 items
+    : Array_ extends {[9]?: unknown}
+        ? Array_ extends readonly [...infer TenOptionalElements extends Optional10UnknownElements, ...any]
+            ? Array_ extends readonly [...Optional10UnknownElements, ...infer RestElements]
+                ? GetAllOptionalElementsOnLeftSide<RestElements, [...Accumulator, ...TenOptionalElements]>
+                : never
+            : never
+    // get the first item
+	: Array_ extends {[0]?: unknown}
+		? Array_ extends readonly [(infer Head)?, ...infer RestElements]
+			? GetAllOptionalElementsOnLeftSide<RestElements, [...Accumulator, ...[Head?]]>
+			: never
+	: [Accumulator, Array_];
+
+type GetAllElementsOnLeftSide<Array_ extends UnknownArray> =
+	GetAllRequiredElementsOnLeftSide<Array_> extends [infer RequiredElements extends UnknownArray, infer OptionalElementsAndRest extends UnknownArray]
+	? GetAllOptionalElementsOnLeftSide<OptionalElementsAndRest> extends [infer OptionalElements extends UnknownArray, infer RestAtStart extends UnknownArray]
+		? [RequiredElements, OptionalElements, RestAtStart]
+		: never
+	: never
+
+type GetAllRequiredElementsOnRightSide<Array_ extends UnknownArray, Accumulator extends UnknownArray = []> = 
+	Array_ extends readonly [...any, ...infer ThousandRequiredElements extends Required1000UnknownElements]
+	? Array_ extends readonly [...infer RestElements, ...Required1000UnknownElements]
+		? GetAllRequiredElementsOnRightSide<RestElements, [...ThousandRequiredElements, ...Accumulator]>
+		: never
+	: Array_ extends readonly [...any, ...infer HundredRequiredElements extends Required100UnknownElements]
+		? Array_ extends readonly [...infer RestElements, ...Required100UnknownElements]
+			? GetAllRequiredElementsOnRightSide<RestElements, [...HundredRequiredElements, ...Accumulator]>
+			: never
+	: Array_ extends readonly [...any, ...infer TenRequiredElements extends Required10UnknownElements]
+		? Array_ extends readonly [...infer RestElements, ...Required10UnknownElements]
+			? GetAllRequiredElementsOnRightSide<RestElements, [...TenRequiredElements, ...Accumulator]>
+			: never
+	: Array_ extends readonly [...infer RestElements, infer Last]
+		? GetAllRequiredElementsOnRightSide<RestElements, [Last, ...Accumulator]>
+	: [Accumulator, Array_]
+
+type TransformOptionalElementsByOptions<
+	OptionalElements extends UnknownArray, 
+	Options extends Required<SplitOnRestElementOptions>
+> = 
+	Options["preserveOptionalModifier"] extends true
+	? OptionalElements
+	: IsExactOptionalPropertyTypesEnabled extends true
+		? Required<OptionalElements>
+		: {[Key in keyof OptionalElements]-?: OptionalElements[Key] | undefined}
+
 export type _SplitOnRestElement<
 	Array_ extends UnknownArray,
 	Options extends Required<SplitOnRestElementOptions>,
-	HeadAcc extends UnknownArray = [],
-	TailAcc extends UnknownArray = [],
-> =
-	keyof Array_ & `${number}` extends never
-		// Enters this branch, if `Array_` is empty (e.g., []),
-		// or `Array_` contains no non-rest elements preceding the rest element (e.g., `[...string[]]` or `[...string[], string]`).
-		? Array_ extends readonly [...infer Rest, infer Last]
-			? _SplitOnRestElement<Rest, Options, HeadAcc, [Last, ...TailAcc]> // Accumulate elements that are present after the rest element.
-			: [HeadAcc, Array_ extends readonly [] ? [] : Array_, TailAcc] // Add the rest element between the accumulated elements.
-		: Array_ extends readonly [(infer First)?, ...infer Rest]
-			? _SplitOnRestElement<
-				Rest, Options,
-				[
-					...HeadAcc,
-					...IsOptionalKeyOf<Array_, '0'> extends true
-						? Options['preserveOptionalModifier'] extends false
-							? [If<IsExactOptionalPropertyTypesEnabled, First, First | undefined>] // Add `| undefined` for optional elements, if `exactOptionalPropertyTypes` is disabled.
-							: [First?]
-						: [First],
-				],
-				TailAcc
-			> // Accumulate elements that are present before the rest element.
-			: never; // Should never happen, since `[(infer First)?, ...infer Rest]` is a top-type for arrays.
+> = 
+	number extends Array_["length"]
+		? GetAllRequiredElementsOnRightSide<Array_> extends [infer LastElements extends UnknownArray, infer FrontElementsAndRest extends UnknownArray]
+			? LastElements extends []
+				? GetAllElementsOnLeftSide<FrontElementsAndRest> extends [
+					infer RequiredElements extends UnknownArray, 
+					infer OptionalElements extends UnknownArray, 
+					infer RestElement extends UnknownArray // is now a true array
+				]
+					? [
+						[...RequiredElements, ...TransformOptionalElementsByOptions<OptionalElements, Options>]
+						,RestElement, LastElements
+					] extends infer Value extends [UnknownArray, UnknownArray, UnknownArray]
+						? If<IsArrayReadonly<Array_>, Readonly<Value>, Value>
+						: never
+					: never
+			: GetAllRequiredElementsOnLeftSide<FrontElementsAndRest> extends [infer FrontRequiredElements extends UnknownArray, infer RestElement extends UnknownArray]
+				? [FrontRequiredElements, RestElement, LastElements] extends infer Value extends [UnknownArray, UnknownArray, UnknownArray]
+					? If<IsArrayReadonly<Array_>, Readonly<Value>, Value>
+					: never
+				: never
+			: never
+		: GetAllElementsOnLeftSide<Array_> extends [
+			infer RequiredElements extends UnknownArray, 
+			infer OptionalElements extends UnknownArray, 
+			infer RestElement extends UnknownArray // is really just []
+		]
+			? [
+				[...RequiredElements, ...TransformOptionalElementsByOptions<OptionalElements, Options>]
+				,RestElement, []
+			] extends infer Value extends [UnknownArray, UnknownArray, UnknownArray]
+				? If<IsArrayReadonly<Array_>, Readonly<Value>, Value>
+				: never
+			: never
 
 export {};
